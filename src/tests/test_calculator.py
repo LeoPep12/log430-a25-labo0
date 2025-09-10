ff47c917fd8f239a4280461a9a2765a9e4f3659c
LeoPep12 <157128614+LeoPep12@users.noreply.github.com>
2025-09-10 11:45:33 -0400

    Ajouts finaux

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -25,4 +25,5 @@ def test_multiplication():
 def test_division():
     my_calculator = Calculator()
     assert my_calculator.division(6, 2) == 3
+    
     assert my_calculator.division(5, 0) == "Erreur : division par zéro" 
